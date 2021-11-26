d3fd891b037686e5ab33dbe5f02867ce8d467106
Noah <noa316@naver.com>
2021-11-26 19:58:32 +0900

    ➕[ADD] : 이미지 const 추가

---
+++ NaverBlog-iOS/NaverBlog-iOS/Resources/Consts/Image.swift
@@ -0,0 +1,52 @@
+//
+//  Image.swift
+//  NaverBlog-iOS
+//
+//  Created by Noah on 2021/11/19.
+//
+
+import UIKit
+
+// MARK: - Image
+extension Const {
+	struct Image {
+		
+		/// Image for Feed
+		
+		static let imgMain = UIImage(named: "imgMain")
+		static let imgPost1 = UIImage(named: "imgPost1")
+		static let imgPost2 = UIImage(named: "imgPost2")
+		static let imgPost3 = UIImage(named: "imgPost3")
+		static let infoPhoto = UIImage(named: "infoPhoto")
+		static let btnGridGallery = UIImage(named: "btnGridGallery")
+		static let btnGridList1 = UIImage(named: "btnGridList1")
+		static let btnGridList2 = UIImage(named: "btnGridList2")
+		static let icComment = UIImage(named: "icComment")
+		static let icDropdown = UIImage(named: "icDropdown")
+		static let icMenu = UIImage(named: "icMenu")
+		static let icMenuSub = UIImage(named: "icMenuSub")
+		static let icSearch = UIImage(named: "icSearch")
+		static let icShare = UIImage(named: "icShare")
+		static let icStatus = UIImage(named: "icStatus")
+		
+		/// Image for Keyboard
+		
+		static let icCamera = UIImage(named: "icCamera")
+		static let icColorTag = UIImage(named: "icColorTag")
+		static let icEmoticon = UIImage(named: "icEmoticon")
+		static let icLock = UIImage(named: "icLock")
+		static let icTag = UIImage(named: "icTag")
+		
+		/// Image for Navigation
+		
+		static let icBack = UIImage(named: "icBack")
+		
+		/// Image for TabBar
+		
+		static let icAlarm = UIImage(named: "icAlarm")
+		static let icFeed = UIImage(named: "icFeed")
+		static let icProfile = UIImage(named: "icProfile")
+		static let icRecommend = UIImage(named: "icRecommend")
+		static let icWriteStroke = UIImage(named: "icWriteStroke")
+	}
+}
