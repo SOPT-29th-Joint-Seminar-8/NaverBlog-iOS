1b8e0a1c3e452626ba0b632ae667168204d37dd0
Noah <noa316@naver.com>
2021-11-26 19:59:28 +0900

    ➕[ADD] : Formatter, NotificationCenter extension 추가

---
+++ NaverBlog-iOS/NaverBlog-iOS/Resources/Extensions/Formatter+.swift
@@ -0,0 +1,24 @@
+//
+//  DateFormatter+.swift
+//  NaverBlog-iOS
+//
+//  Created by Noah on 2021/11/25.
+//
+
+import Foundation
+
+extension Formatter {
+	static func string(iso date: Date) -> String {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy.M.d"
+	
+		return dateFormatter.string(from: date)
+	}
+	
+	static func stringToDate(date: String) -> Date? {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+		
+		return dateFormatter.date(from: date)
+	}
+}
