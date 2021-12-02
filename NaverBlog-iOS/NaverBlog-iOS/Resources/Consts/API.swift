ef865e96b775e50dee5dcf39bfcd600f76484030
Noah <noa316@naver.com>
2021-12-02 17:51:29 +0900

    ➕[ADD] : Network 관련 Const, enum 추가 - API.swift : Network 관련 URL Const 추가 - NetworkResult.swift : Network Result 관련 enum 추가

---
+++ NaverBlog-iOS/NaverBlog-iOS/Resources/Consts/API.swift
@@ -0,0 +1,44 @@
+//
+//  API.swift
+//  NaverBlog-iOS
+//
+//  Created by Noah on 2021/12/02.
+//
+
+import Foundation
+
+extension Const {
+	struct API {
+		
+		// MARK: - Naver Blog API BASE URL
+		
+		static let baseURL = "https://asia-northeast3-naverblog-server.cloudfunctions.net/api"
+		
+		// MARK: - Feature: get main feed data W/ GET
+		
+		static let getMainFeedURL = baseURL + "/post/main"
+		
+		// MARK: - Get Post's Comments W/ GET
+		
+		static func getPostCommentsURL(postId: String) -> String {
+			return baseURL + "/post/\(postId)/comments"
+		}
+		
+		// MARK: - Send Like to Post W/ PETCH
+		
+		static func sendLikeToPostURL(postId: String, state: Bool) -> String {
+			return baseURL + "/post/\(postId)/like?state=\(state)"
+		}
+		
+		// MARK: - Send Like to Comment W/ PETCH
+		
+		static func sendLikeToCommentURL(commentId: String, state: Bool) -> String {
+			return baseURL + "/comment/\(commentId)/like?state=\(state)"
+		}
+		
+		// MARK: - Post Comment W/ POST
+		
+		static let postCommentURL = baseURL + "/comment"
+		
+	}
+}
